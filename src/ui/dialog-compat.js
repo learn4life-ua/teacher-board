91b369dfbf1d5ec9b1b6bbaf2f85a8f1ef3f9ebe
learn4life <tab10827@gmail.com>
2026-08-16 11:56:05 +0300

    Polyfill minimal dialog open and close behavior

--- src/ui/dialog-compat.js
+++ src/ui/dialog-compat.js
@@ -1,10 +1,29 @@
+function dialogPrototype(){
+  if(typeof HTMLDialogElement!=='undefined')return HTMLDialogElement.prototype;
+  if(typeof HTMLElement!=='undefined')return HTMLElement.prototype;
+  return null;
+}
+
+export function ensureDialogCompatibility(){
+  const proto=dialogPrototype();
+  if(!proto)return;
+  if(typeof proto.showModal!=='function'){
+    Object.defineProperty(proto,'showModal',{configurable:true,writable:true,value:function(){this.setAttribute?.('open','');}});
+  }
+  if(typeof proto.close!=='function'){
+    Object.defineProperty(proto,'close',{configurable:true,writable:true,value:function(returnValue=''){
+      this.returnValue=String(returnValue||'');
+      this.removeAttribute?.('open');
+      try{this.dispatchEvent?.(new Event('close'));}catch{}
+    }});
+  }
+}
+
 export function openDialog(dialog){
   if(!dialog)return false;
   if(typeof dialog.showModal==='function'){
     if(!dialog.open)dialog.showModal();
-  }else{
-    dialog.setAttribute('open','');
-  }
+  }else dialog.setAttribute('open','');
   return true;
 }
 
@@ -18,3 +37,5 @@ export function closeDialog(dialog,returnValue=''){
   }
   return true;
 }
+
+ensureDialogCompatibility();
